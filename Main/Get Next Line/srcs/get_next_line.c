58c70cfd8f79bd89b7f7fff8e5d2b50c5c4a879c
Sino- <SinisterSino@gmail.com>
2017-03-27 14:26:19 -0700

    cleaned up a bit

--- Main/Get Next Line/srcs/get_next_line.c
+++ Main/Get Next Line/srcs/get_next_line.c
@@ -6,27 +6,20 @@ int	get_next_line(const int fd, char **line)
 	static char	**leftovers;
 	int			split_pt;
 	int 		bytes_read;
-	char		*temp_leftovers;
+	char		*tmp;
 	char		buffer[BUFF_SIZE + 1];
 
-	split_pt = -1;
 	if (!leftovers)
-	{
 		leftovers = (char **)malloc(sizeof(char *) * (MAX_FD + 1));
-		if (!leftovers)
-			return (1);
-		leftovers[split_pt] = NULL;
-	}
-
 	split_pt = 0;
 	while((bytes_read = read(fd, buffer, BUFF_SIZE)) > 0)
 	{
 		buffer[bytes_read] = '\0';
 		if (leftovers[fd])
 		{
-			temp_leftovers = ft_strjoin(leftovers[fd], buffer);
+			tmp = ft_strjoin(leftovers[fd], buffer);
 			free(leftovers[fd]);
-			leftovers[fd] = temp_leftovers;
+			leftovers[fd] = tmp;
 		}
 		else
 			leftovers[fd] = ft_strdup(buffer);
@@ -35,9 +28,9 @@ int	get_next_line(const int fd, char **line)
 		if (leftovers[fd][split_pt] == 10)
 		{
 			*line = ft_strsub(leftovers[fd], 0, split_pt);
-			temp_leftovers = ft_strsub(leftovers[fd], (split_pt + 1), ft_strlen(leftovers[fd]) - split_pt);
+			tmp = ft_strsub(leftovers[fd], (split_pt + 1), ft_strlen(leftovers[fd]) - split_pt);
 			free(leftovers[fd]);
-			leftovers[fd] = temp_leftovers;
+			leftovers[fd] = tmp;
 			return (1);
 		}
 	}
